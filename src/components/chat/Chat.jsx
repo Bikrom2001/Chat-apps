3528a239221aca743ab6df0c0be2e0b4234f53c6
Bikrom2001 <bikromroy2001@gmail.com>
2024-11-22 12:27:27 +0600

    React.js How to Add Emoji? (React Emoji Picker)

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,60 @@
+import EmojiPicker from "emoji-picker-react";
+import { useState } from "react";
 import "./chat.css";
-
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
+  console.log(text);
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="avatar" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor, sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="img" />
+          <img src="./camera.png" alt="camera" />
+          <img src="./mic.png" alt="mic" />
+        </div>
+        <input
+          type="text"
+          value={text}
+          placeholder="Type a message..."
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt="emoji"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton"> Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
